eaa0925a0fde27e9a1706e058ddf83eda7529eab
ahmadrmdhann <ahmadramadhansqorpio04@gmail.com>
2025-02-24 14:57:50 +0700

    Add KategoriController and view for managing categories, update DatabaseSeeder to include KategoriSeeder, and define route for categories

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TransactionController;
 use App\Http\Controllers\LevelController;
@@ -23,3 +24,4 @@ Route::get('/', function () {
 });
 
 Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
