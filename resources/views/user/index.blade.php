716b070d6ac9598972507d6dbcd0fca499ebb850
ahmadrmdhann <ahmadramadhansqorpio04@gmail.com>
2025-03-16 12:32:44 +0700

    Add AJAX user creation functionality and validation

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -6,14 +6,16 @@
             <h3 class="card-title">{{ $page->title }}</h3>
             <div class="card-tools">
                 <a class="btn btn-sm btn-primary mt-1" href="{{ url('user/create') }}">Tambah</a>
+                <button onclick="modalAction('{{ url('/user/create_ajax') }}')" class="btn btn-sm btn-success mt-1">Tambah
+                    Ajax</button>
             </div>
         </div>
         <div class="card-body">
             @if (session('success'))
-            <div class="alert alert-success">{{ session('success') }}</div>
+                <div class="alert alert-success">{{ session('success') }}</div>
             @endif
             @if (session('error'))
-            <div class="alert alert-danger">{{ session('error') }}</div>
+                <div class="alert alert-danger">{{ session('error') }}</div>
             @endif
             <div class="row">
                 <div class="col-md-12">
@@ -23,7 +25,7 @@
                             <select class="form-control" name="level_id" id="level_id" required>
                                 <option value="">- Semua -</option>
                                 @foreach($level as $item)
-                                <option value="{{ $item->level_id }}">{{ $item->level_nama }}</option>
+                                    <option value="{{ $item->level_id }}">{{ $item->level_nama }}</option>
                                 @endforeach
                             </select>
                             <small class="form-text text-muted">Level Pengguna</small>
@@ -33,11 +35,18 @@
             </div>
             <table class="table table-bordered table-striped table-hover table-sm" id="table_user">
                 <thead>
-                    <tr><th>ID</th><th>Username</th><th>Nama</th><th>Level Pengguna</th><th>Aksi</th></tr>
+                    <tr>
+                        <th>ID</th>
+                        <th>Username</th>
+                        <th>Nama</th>
+                        <th>Level Pengguna</th>
+                        <th>Aksi</th>
+                    </tr>
                 </thead>
             </table>
         </div>
     </div>
+    <div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog" data-backdrop="static" data-keyboard="false" data-width="75%" aria-hidden="true"></div>
 @endsection
 
 @push('css')
@@ -45,7 +54,14 @@
 
 @push('js')
     <script>
-        $(document).ready(function() {
+        function modalAction(url = '') {
+            $('#myModal').load(url, function () {
+                $('#myModal').modal('show');
+            });
+        }
+
+        var dataUser;
+        $(document).ready(function () {
             var dataUser = $('#table_user').DataTable({
                 serverSide: true,
                 ajax: {
@@ -91,7 +107,7 @@
                     }
                 ]
             });
-            $('#level_id').change(function() {
+            $('#level_id').change(function () {
                 dataUser.ajax.reload();
             });
         });
