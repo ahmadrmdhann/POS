d6e447cf246edd9b31cb0918f1dfc97082823001
ahmadrmdhann <ahmadramadhansqorpio04@gmail.com>
2025-03-18 20:27:23 +0700

    FIX: correct typo in breadcrumb title in WelcomeController

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -8,7 +8,7 @@ class WelcomeController extends Controller
     public function index()
     {
         $breadcrumb = (object)[
-            'tittle' => 'Selamat Datang',
+            'title' => 'Selamat Datang',
             'list' => ['Home', 'Welcome'],
         ];
 
