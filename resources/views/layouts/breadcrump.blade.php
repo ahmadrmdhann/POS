ef55be23f26cc9ea10dc53a803382a8a10dc244e
ahmadrmdhann <ahmadramadhansqorpio04@gmail.com>
2025-03-18 12:01:42 +0700

    FIX: dynamic breadcrumb navigation with conditional rendering based on breadcrumb data

--- resources/views/layouts/breadcrump.blade.php
+++ resources/views/layouts/breadcrump.blade.php
@@ -1,15 +1,18 @@
-<section class="content-header">
-    <div class="container-fluid">
-      <div class="row mb-2">
-        <div class="col-sm-6">
-          <h1>Blank Page</h1>
-        </div>
-        <div class="col-sm-6">
-          <ol class="breadcrumb float-sm-right">
-            <li class="breadcrumb-item"><a href="#">Home</a></li>
-            <li class="breadcrumb-item active">Blank Page</li>
-          </ol>
-        </div>
-      </div>
-    </div><!-- /.container-fluid -->
-  </section>
+@if(!empty($breadcrumb))
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>{{ $breadcrumb->title }}</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        @foreach ($breadcrumb->list as $item)
+                            <li class="breadcrumb-item"><a href="#">{{ $item }}</a></li>
+                        @endforeach
+                    </ol>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+@endif
